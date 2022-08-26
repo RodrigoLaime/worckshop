504543a024ce369927512726cea4a155b4ab9ee8
rodrigo laime <rodrigolaime141@gmail.com>
2022-08-26 17:45:17 -0300

    segundo-commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@
 
 const baseUrl = "https://platzi-avo.vercel.app";
 const appNode = document.querySelector('#app'); //seleccionamos la etiqueta donde vamos a guardar
+appNode.addEventListener("click", () => {
+    window.alert("Hola");
+});
 window.fetch(`${baseUrl}/api/avo`) //conectamnos con la API
 .then((respuesta) => respuesta.json())//procesamos la respuestza y la convertimos json
 .then((responseJson) => {//json(data). renderisamos la informacion en browser
@@ -12,12 +15,15 @@ window.fetch(`${baseUrl}/api/avo`) //conectamnos con la API
     responseJson.data.forEach((item) => {
         const imagen = document.createElement('img');
         imagen.src = `${baseUrl}${item.image}`;
+
         const title = document.createElement("h2");
         title.textContent = item.name; //contenidoTexto será igual al name del ítem de data
         title.style = "font-size: 2rem";
+
         const price = document.createElement("div");
         price.className = "text-gray-600"; //
-        price.textContent = formatPrice(item.price);
+        price.textContent = `${item.price}`;
+
         const container = document.createElement("div") //creamos un contendor
         container.append(imagen, title, price); //agregamos los elementos al contendor
         todosLosItems.push(container) //agrega el contendor a esta varialble 
